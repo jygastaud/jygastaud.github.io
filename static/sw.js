05f8e13453867abe786bf0d679ddb06b8ee76eab
Jean-Yves Gastaud <jygastaud@mail.com>
2019-04-30 08:56:10 +0200

    fix SW

--- static/sw.js
+++ static/sw.js
@@ -49,7 +49,7 @@ const MAX_TTL = {
 
 const CACHE_BLACKLIST = [
     (str) => {
-       return !str.startsWith('http://localhost'); //&& !str.startsWith('https://gastaud.io');
+       return !str.startsWith('https://gastaud.io');
     },
 ];
 
